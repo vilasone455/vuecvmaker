62a5f7dd5d77f2792eb9a184d3c2b59515856875
vilasone455 <64119057+vilasone455@users.noreply.github.com>
2020-07-15 22:18:42 +0700

    replace css framework from vuematerial to mdbvue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "bootstrap-vue/dist/bootstrap-vue.css";
-import BootstrapVue from "bootstrap-vue";
-Vue.config.productionTip = false;
 
-Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 //
 new Vue({
   render: h => h(App),
